a82b2e8b1029dfe51f0c963cd8f1be7fd91b591d
DanielEizenberger <daniel0eizenberger@gmail.com>
2021-06-27 15:10:08 +0200

    Random Stuff

--- src/net/htlgkr/handler/GraphmlHandler.java
+++ src/net/htlgkr/handler/GraphmlHandler.java
@@ -4,10 +4,7 @@ import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class GraphmlHandler extends DefaultHandler {
 
@@ -29,9 +26,16 @@ public class GraphmlHandler extends DefaultHandler {
     private Path currentPath;
     private boolean edge = false;
 
+    //DEBUG
+    private List<String> debug = new ArrayList<>();
+
     @Override
     public void characters(char[] ch, int start, int length) {
-        elementValue = new String(ch, start, length);
+        elementValue += new String(ch, start, length);
+        if(elementValue.trim().equals("null")){
+            elementValue = elementValue.replace("null", "");
+        }
+        elementValue = elementValue.replace("\n", "").trim();
     }
 
     @Override
@@ -41,7 +45,62 @@ public class GraphmlHandler extends DefaultHandler {
 
     @Override
     public void endDocument() throws SAXException {
+        debug();
+        sortNodes();
+
         System.out.println("DEBUG: Reading document stopped!"); // For debug purposes
+    }
+
+    private void debug(){
+        debug.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return Integer.parseInt(o1.split(" ")[0]) - Integer.parseInt(o2.split(" ")[0]);
+            }
+        });
+
+        /*List<String> temp = new ArrayList<>();
+        temp.add(debug.get(0));
+        debug.remove(0);
+        for (int i = 0; i < temp.size(); i++){
+            for(int a = 0; a < debug.size(); a++){
+                if(temp.get(i).split(" ")[2].equals(debug.get(a).split(" ")[0])){
+                    temp.add(debug.get(a));
+                    debug.remove(a);
+                }
+            }
+        }*/
+
+
+        for(String s : debug){
+            System.out.println(s);
+        }
+    }
+
+    private void sortNodes(){
+        Node[] no = new Node[nodes.size()];
+        int counter = 1;
+
+        int from = Integer.parseInt(debug.get(0).split(" ")[0]);
+        no[counter-1] = (Node) nodes.toArray()[from];
+        int to = Integer.parseInt(debug.get(0).split(" ")[2]);
+
+        for (int i = 0; i < debug.size(); i++){
+            if(counter < nodes.size()) no[counter] = (Node) nodes.toArray()[to-1];
+
+            for(String s : debug){
+                if(Integer.parseInt(s.split(" ")[0]) == to){
+                    to = Integer.parseInt(s.split(" ")[2]);
+                    break;
+                }
+            }
+
+            counter++;
+        }
+
+        for (Node n : no){
+            System.out.println(n.getNodeLabel());
+        }
     }
 
     @Override
@@ -50,6 +109,9 @@ public class GraphmlHandler extends DefaultHandler {
             case NODE:
                 String[] temp = attr.getValue(0).split("n");
                 currentNodeID = temp[temp.length-1];
+
+                if(attr.getValue(0).equals("n0")) currentNodeID = "-1";
+                System.out.println(attr.getValue(0) + " | " + currentNodeID);
                 break;
             case NODELABEL:
                 label = true;
@@ -63,6 +125,8 @@ public class GraphmlHandler extends DefaultHandler {
                 currentEdgeSourceID = temp1[temp1.length-1];
                 String[] temp2 = attr.getValue(2).split("n");
                 currentEdgeTargetID = temp2[temp2.length-1];
+
+                debug.add(currentEdgeSourceID + " -> " + currentEdgeTargetID + " | " + attr.getValue(0));
                 break;
             case PATH:
                 currentPath = new Path();
